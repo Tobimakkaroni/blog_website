7b111e462818311224c2bce65ce99badcfe77b11
Tobias <tobiasmailphone@gmail.com>
2024-04-20 15:59:23 +0200

    working comment writing and preview but bug with comment sending

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django_comments_xtd import urls as django_comments_xtd_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('comments/', include(django_comments_xtd_urls)),
 ]
