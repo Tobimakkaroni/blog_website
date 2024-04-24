2545b1afb131864089216200e51f7d050f0826f6
Tobimakkaroni <tobiasmailphone@gmail.com>
2024-04-24 14:28:22 +0200

    wip not finished

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Feature(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
