f18b268b832f0c876bd5707483a3df57c3cac9f0
tomatensaft-admin <tobiasmailphone@gmail.com>
2024-04-29 12:28:13 +0200

    link back home from rust page

--- rust/static/rust/js/rust_calc.js
+++ rust/static/rust/js/rust_calc.js
@@ -1,4 +1,4 @@
-function calculate() {
+function calculateC4() {
     var inputc4 = parseInt(document.getElementById("inputc4").value);
     var gp = 0;
     var metal = 0;
@@ -26,5 +26,36 @@ function calculate() {
 
         var result = "C4 produced! gp: " + gp + ", metal: " + metal + ", sulf: " + sulf + ", granades: " + (granades + roundG) + ", scrap: " + (scrap + (roundG * 5));
         document.getElementById("result").innerHTML += "<p>" + result + "</p>";
+    }
+}
+
+function calculateRocket() {
+    var inputRocket = parseInt(document.getElementById("inputRocket").value);
+    var gp = 0;
+    var metal = 0;
+    var sulf = 0;
+    var granades = 0;
+    var scrap = 0;
+    var roundG = inputRocket
+
+    while (inputRocket > 0) {
+        while (gp < 1000 || metal < 200 || sulf < 200) {
+            gp += 18;
+            metal += 25;
+            granades++;
+
+            while (sulf < 200 && gp > 1000) {
+                gp -= 1;
+                sulf += 1;
+            }
+        }
+        gp -= 1000;
+        metal -= 200;
+        sulf -= 200;
+        inputc4 -= 1;
+        scrap = granades * 5;
+
+        var result = "Rocket produced! gp: " + gp + ", metal: " + metal + ", sulf: " + sulf + ", granades: " + (granades + roundG) + ", scrap: " + (scrap + (roundG * 5));
+        document.getElementById("resultRocket").innerHTML += "<p>" + result + "</p>";
     }
 }
